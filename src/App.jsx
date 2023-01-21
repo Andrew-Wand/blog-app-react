672857baefe99f900d96c2a1cd3a02708db1a83d
Andrew Wand <andrewwands@gmail.com>
2023-01-21 16:53:54 -0600

    pages and components set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import AddPost from "./pages/AddPost";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<SignIn />} />
+          <Route path="/add-post" element={<AddPost />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
